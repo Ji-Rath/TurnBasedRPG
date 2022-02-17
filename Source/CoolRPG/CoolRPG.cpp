bf2b91b8922a746ad4ddaa450a0435a1ab01a6db
BigGravy <gargsr@frontier.com>
2022-02-16 23:25:52 -0500

    Add initial files

---
+++ Source/CoolRPG/CoolRPG.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CoolRPG.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CoolRPG, "CoolRPG" );
